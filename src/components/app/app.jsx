ca9a02b3c4a33394547e4021aa6221a8ff31597a
SeliverstovDanila <Catsvill99@yandex.ru>
2023-09-22 17:59:05 +0400

    add burger ingredients ver 1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import { AppHeader } from "../app-header/app-header";
-
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 
 function App() {
 
@@ -10,6 +10,9 @@ function App() {
       <header>
         <AppHeader />
       </header>
+      <main className={styles.main}>
+        <BurgerIngredients compound={data} />
+      </main>
     </>
   );
 }
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types'
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import {ingredientPropType} from '../../utils/prop-types'
+import styles from "./burger-ingredients.module.css";
+import IngredientGroup from "../burger-ingredients/ingredient-group/ingredient-group.jsx";
+
+function BurgerIngredients({compound}) {
+    const [current, setCurrent] = useState('buns')
+
+    const buns = compound.filter(item => item.type === 'buns')
+    const sauce = compound.filter(item => item.type === 'sauce')
+    const main = compound.filter(item => item.type === 'main')
+
+    const bunsRef = useRef(null)
+    const sauceRef = useRef(null)
+    const mainRef = useRef(null)
+
+    const handleTab = (value) => {
+        setCurrent(value)
+        if (value === "buns") bunsRef.current.scrollIntoView({ behavior: "smooth" })
+        if (value === "sauce") sauceRef.current.scrollIntoView({ behavior: "smooth" })
+        if (value === "main") mainRef.current.scrollIntoView({ behavior: "smooth" })
+    };
+    return (
+        <section className={`styles.burgerIngredients`}>
+            <p className={`text text_type_main-large pt-10 pb-5`}>Соберите бургер</p>
+            <div className={`styles.tab_panel mb-10 pb-40`}>
+                <Tab value="buns" active={current === 'buns'} onClick={handleTab}>
+                    Булки
+                </Tab>
+                <Tab value="sauce" active={current === 'sauce'} onClick={handleTab}>
+                    Соусы
+                </Tab>
+                <Tab value="main" active={current === 'main'} onClick={handleTab}>
+                    Начинки
+                </Tab>
+            </div>
+            <ul className={`${styles.burgerIngredients_items} custom-scroll`}>
+                <li ref={bunsRef}>
+                    <IngredientGroup
+                        name='Булки'
+                        compound={buns}
+                    />
+                </li>
+                <li ref={sauceRef}>
+                    <IngredientGroup
+                        name='Соусы'
+                        compound={sauce}
+                    />
+                </li>
+                <li ref={mainRef}>
+                    <IngredientGroup
+                        name='Начинка'
+                        compound={main}
+                    />
+                </li>
+            </ul>
+        </section>
+    );
+}
+BurgerIngredients.propTypes = {
+    compound: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired,
+}
+
+export default BurgerIngredients;
